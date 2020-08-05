afb8d3fd6719f62fc5b6962a7db44acf55d414d8
satoha <satoha3108biz@gmail.com>
2020-08-05 17:29:19 +0900

    管理画面作成中

---
+++ components/Mglobalnav.vue
@@ -0,0 +1,59 @@
+<template>
+  <div role="tablist">
+    <b-button v-b-toggle.accordion-1 block variant="info">BLOG管理</b-button>
+    <b-collapse
+      id="accordion-1"
+      visible
+      accordion="my-accordion"
+      role="tabpanel"
+    >
+      <b-card>記事一覧</b-card>
+      <b-card>新規作成：記事</b-card>
+    </b-collapse>
+    <b-button v-b-toggle.accordion-2 block variant="info">問題集管理</b-button>
+    <b-collapse
+      id="accordion-2"
+      visible
+      accordion="my-accordion"
+      role="tabpanel"
+    >
+      <b-card>コース一覧</b-card>
+      <b-card>新規作成：コース</b-card>
+    </b-collapse>
+    <b-button v-b-toggle.accordion-3 block variant="info">画像管理</b-button>
+    <b-collapse
+      id="accordion-3"
+      visible
+      accordion="my-accordion"
+      role="tabpanel"
+    >
+      <b-card>画像一覧</b-card>
+      <b-card>新規アップロード</b-card>
+    </b-collapse>
+    <b-button v-b-toggle.accordion-4 block variant="info">サイト管理</b-button>
+    <b-collapse
+      id="accordion-4"
+      visible
+      accordion="my-accordion"
+      role="tabpanel"
+    >
+      <b-card>サイト分析</b-card>
+      <b-card>管理者アカウント</b-card>
+    </b-collapse>
+  </div>
+</template>
+<style lang="scss" scoped>
+$pri-color: #41b883;
+$scd-color: #35495e;
+$w: white;
+button {
+  background-color: $scd-color;
+  margin-top: 5px;
+}
+.card {
+  &:hover {
+    background-color: $pri-color;
+  }
+  cursor: pointer;
+}
+</style>
---
+++ pages/manage/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Header></Header>
+    <b-container>
+      <b-row>
+        <b-col sm="4">
+          <Mgnav></Mgnav>
+        </b-col>
+        <b-col sm="8">
+          試作中
+        </b-col>
+      </b-row>
+    </b-container>
+    <Footer></Footer>
+  </div>
+</template>
+<script>
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+import Mgnav from '../../components/Mglobalnav'
+export default {
+  components: {
+    Header,
+    Footer,
+    Mgnav,
+  },
+}
+</script>
